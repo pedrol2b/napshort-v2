4533a49b1a780760f53edc6b9096ce3d065b7241
Pedro Bueno <pedrolbb.e@gmail.com>
2023-03-01 20:13:02 -0300

    fix: add types to firebase/sdk in config/

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,23 @@
 import { readdirSync, readFileSync } from 'fs'
 import { resolve } from 'path'
 
-const projectRoot = resolve(__dirname, '../', '../')
-const SDKPath = readdirSync(projectRoot).filter((file) => file.includes('firebase-adminsdk') && file.endsWith('.json'))
+export interface ISDK {
+  type: string
+  project_id: string
+  private_key_id: string
+  private_key: string
+  client_email: string
+  client_id: string
+  auth_uri: string
+  token_uri: string
+  auth_provider_x509_cert_url: string
+  client_x509_cert_url: string
+}
 
-const SDK = JSON.parse(readFileSync(resolve(projectRoot, SDKPath[0]), 'utf-8'))
+const projectRoot = resolve(__dirname, '../..')
+const SDKPath = readdirSync(projectRoot).filter((file) => file.includes('firebase-adminsdk') && file.endsWith('.json'))
+const SDKFile = SDKPath[0]
+
+const SDK: ISDK = JSON.parse(readFileSync(resolve(projectRoot, SDKFile), 'utf-8'))
 
 export { SDK }

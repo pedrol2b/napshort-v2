1dc0786eb16f2e5fd1b43ed7ec6f75bd0cc39929
Pedro Bueno <pedrolbb.e@gmail.com>
2023-03-01 19:56:47 -0300

    feat: removed controller to test Port

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,5 @@
 import { Router } from 'express'
-import { HelloWorldController } from '@/controllers/HelloWorldController'
 
-const helloWorldController = new HelloWorldController()
 const router = Router()
-
-router.get('/', helloWorldController.default)
 
 export default router

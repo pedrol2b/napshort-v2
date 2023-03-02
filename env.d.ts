03c406c2852e4cff3e202e227044aceb46ecebd2
Pedro Bueno <pedrolbb.e@gmail.com>
2023-03-02 10:28:38 -0300

    chore: add CLOUD_STORAGE_ to env types

--- env.d.ts
+++ env.d.ts
@@ -4,6 +4,8 @@ declare global {
       readonly PORT?: string
       readonly MONGO_URL?: string
       readonly MONGO_DB?: string
+      readonly CLOUD_STORAGE_API?: string
+      readonly CLOUD_STORAGE_BUCKET?: string
     }
   }
 }

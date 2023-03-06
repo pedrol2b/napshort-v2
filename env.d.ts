47b031617b6020037a5589aa3dc9f13cd93cc324
Pedro Bueno <pedrolbb.e@gmail.com>
2023-03-06 16:43:29 -0300

    fix: change PORT to type number in env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      readonly PORT?: string
+      readonly PORT?: number
       readonly MONGO_URL?: string
       readonly MONGO_DB?: string
       readonly CLOUD_STORAGE_API?: string
